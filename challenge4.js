44cfbe72d43e879ebc59842790cae80947e2eb4e
Elmer Balbin Jr <contact@elmer.dev>
2023-09-13 16:08:55 -0700

    Updated challenge 5

--- challenge4.js
+++ challenge4.js
@@ -4,7 +4,8 @@
  * Pseudocode:
  * 1. Input car name (carName)
  * 2. Input year of purchase (carYear)
- * 3. If year of purchase is greater than 2020, then print message "carYear carName is NEW!". Otherwise, print message "carYear carName is OLD!"
- * (Example output: "2004 Volvo is OLD!")
+ * 3. If year of purchase is greater than 2020, then print message "My carYear carName is NEW!". Otherwise, print message "My carYear carName is OLD!"
+ * 
+ * (Example output: "My 2004 Volvo is OLD!")
  * 
  */
